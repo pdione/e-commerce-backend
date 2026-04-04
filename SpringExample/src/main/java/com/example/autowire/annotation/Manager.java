d0d55c71f101235b66e56a3792cdb3d77d8601a6
Pape Dione <pape.dione@orange-sonatel.com>
2026-04-04 16:14:09 +0000

    @Qualifier Annotaion.

--- SpringExample/src/main/java/com/example/autowire/annotation/Manager.java
+++ SpringExample/src/main/java/com/example/autowire/annotation/Manager.java
@@ -1,12 +1,14 @@
 package com.example.autowire.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Manager {
 
     @Autowired
+    @Qualifier("employee") // When multiple beans of the same type, use @Qualifier to specify which bean to inject
     private Employee employee;
 
 //    @Autowired
