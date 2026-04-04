b80dff8e7136c192e30fdf580840259e8b4da55a
Pape Dione <pape.dione@orange-sonatel.com>
2026-04-04 16:11:39 +0000

    @Autowired Annotaion.

---
+++ SpringExample/src/main/java/com/example/autowire/annotation/Manager.java
@@ -0,0 +1,23 @@
+package com.example.autowire.annotation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Manager {
+
+    @Autowired
+    private Employee employee;
+
+//    @Autowired
+//    public Manager(Employee employee) {
+//        this.employee = employee;
+//    }
+
+    @Override
+    public String toString() {
+        return "Manager{" +
+                "employee=" + employee +
+                '}';
+    }
+}
