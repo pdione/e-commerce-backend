e1fd26a23fb211d560a2b57468a6f8b76f9dafab
Pape Dione <pape.dione@orange-sonatel.com>
2026-04-04 15:16:15 +0000

    Transition from XML to Annotaion.

--- SpringExample/src/main/java/com/example/componentScan/App.java
+++ SpringExample/src/main/java/com/example/componentScan/App.java
@@ -1,14 +1,14 @@
 package com.example.componentScan;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
 
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("componentScanExample.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         Employee employee = context.getBean("employee", Employee.class);
-        System.out.println(employee.toString());
+        System.out.println(employee);
 
     }
 }
