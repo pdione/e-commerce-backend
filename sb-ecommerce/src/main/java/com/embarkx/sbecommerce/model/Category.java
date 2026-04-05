e8ac5209f3470d83f3c87392c2c9709644d960c2
Pape Dione <pape.dione@orange-sonatel.com>
2026-04-05 18:21:31 +0000

    Add Generation Identity Type.

--- sb-ecommerce/src/main/java/com/embarkx/sbecommerce/model/Category.java
+++ sb-ecommerce/src/main/java/com/embarkx/sbecommerce/model/Category.java
@@ -1,13 +1,26 @@
 package com.embarkx.sbecommerce.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
+
     private String categoryName;
 
     public Category(Long categoryId, String categoryName) {
         this.categoryId = categoryId;
         this.categoryName = categoryName;
+    }
+
+    public Category() {
+
     }
 
     public Long getCategoryId() {
