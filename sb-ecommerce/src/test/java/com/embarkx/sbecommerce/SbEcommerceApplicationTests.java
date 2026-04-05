85b8f6ed74651e2ebecf6680505eee312f9f5603
Pape Dione <pape.dione@orange-sonatel.com>
2026-04-05 11:04:48 +0000

    Init project.

---
+++ sb-ecommerce/src/test/java/com/embarkx/sbecommerce/SbEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.embarkx.sbecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
