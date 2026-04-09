905da8172a88545d05d5ee8ec79337ec92d598c2
Pape Dione <pape.dione@orange-sonatel.com>
2026-04-09 20:36:40 +0000

    Making Basic authentication STATELESS by removing session

--- Security Demo/src/main/java/com/example/securitydemo/config/SecurityConfig.java
+++ Security Demo/src/main/java/com/example/securitydemo/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -15,6 +16,9 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
         http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        // To make the request stateless and disabling cookies
+        http.sessionManagement(
+                session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         //http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
